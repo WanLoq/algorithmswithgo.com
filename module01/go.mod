3aa7974247a3a65916a99fd636684fabaa1aa9b8
wanloq <mcjinkz@gmail.com>
2024-08-31 20:44:40 +0100

    Reverse done

---
+++ module01/go.mod
@@ -0,0 +1,3 @@
+module algorithmswithgo.com/module01
+
+go 1.23.0
--- module01/reverse.go
+++ module01/reverse.go
@@ -1,11 +1,18 @@
 package module01
 
+import (
+	"slices"
+	"strings"
+)
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
-//   Reverse("cat") => "tac"
-//   Reverse("alphabet") => "tebahpla"
-//
+//	Reverse("cat") => "tac"
+//	Reverse("alphabet") => "tebahpla"
 func Reverse(word string) string {
-	return ""
+	asArray := strings.Split(word, "")
+	slices.Reverse(asArray)
+	output := strings.Join(asArray, "")
+	return output
 }
